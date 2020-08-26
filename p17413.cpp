e0c1372244dbd8062b694eff99092eee0085945b
강준현 <kangjunhyeon@gangjunhyeon-ui-MacBookPro.local>
2020-08-26 23:25:15 +0900

    Create p17413.cpp

---
+++ p17413.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int		main(void)
+{
+	string		str;
+	int		len;
+
+	getline(cin, str);
+	for (int i = 0; i < str.length(); i++) {
+		if (str[i] == '<') {
+			if (i != 0) {
+				int	j = i - 1;
+				while (str[j] != ' ' && str[j] != '>') cout << str[j--];
+			}
+			while (str[i] != '>') cout << str[i++];
+			cout << str[i];
+		}
+		if (str[i] == ' ') {
+			int	j = i - 1;
+			while (str[j] != ' ' && str[j] != '>' && j >= 0) cout << str[j--];
+			cout << " ";
+		}
+	}
+	len = str.length() - 1;
+	while (str[len] != ' ' && str[len] != '>' && len >= 0) cout << str[len--];
+	return (0);
+}
