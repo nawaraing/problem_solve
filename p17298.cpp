b0bb86a325e498e8543d9dbc2a33badb2914bd53
강준현 <kangjunhyeon@gangjunhyeon-ui-MacBookPro.local>
2020-08-27 17:39:56 +0900

    Create p17298.cpp
    solve with stack

---
+++ p17298.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void			ft_recur(stack<int> st, stack<int> printable)
+{
+	int		print_num;
+
+	if (st.size() == 0) return ;
+	while (printable.size() && st.top() >= printable.top()) {
+//		cout << "printable.size : " << printable.size() << "\n";
+		printable.pop();
+	}
+//	cout << "1\n";
+	if (printable.size() == 0) print_num = -1;
+	else print_num = printable.top();
+	printable.push(st.top());
+	st.pop();
+	ft_recur(st, printable);
+	cout << print_num << " ";
+	return ;
+}
+
+int			main(void)
+{
+	int		N, A;
+	stack<int>	st, printable;
+
+	cin >> N;
+	while (N--) {
+		cin >> A;
+		st.push(A);
+	}
+	printable.push(st.top());
+	st.pop();
+	ft_recur(st, printable);
+	cout << "-1";
+	return (0);
+}
