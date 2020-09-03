1bce6c964310321b137fff3343aac3ac089d6cb1
강준현 <kangjunhyeon@gangjunhyeon-ui-MacBookPro.local>
2020-09-03 22:09:23 +0900

    Remake p10844.cpp
    before I make function with recursive
    but this code exceed time limit
    so remake function with bottom-up? maybe..

--- p10844.cpp
+++ p10844.cpp
@@ -1,30 +1,33 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-
-int		ans = 0;
-
-void				ft_recur(int i, int N)
-{
-	if (N == 1) {
-		ans++;
-		return ;
-	}
-	if (i != 0) ft_recur(i - 1, N - 1);
-	if (i != 9) ft_recur(i + 1, N - 1);
-	return ;
-}
 
 int				main(void)
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	int		N;
+	int		N, tmp1, tmp2;
+	vector<int>	nums(10);
 
 	cin >> N;
-	for (int i = 1; i < 10; i++)
-		ft_recur(i, N);
+	fill(nums.begin() + 1, nums.end(), 1);
+	for (int i = 1; i < N; i++) {
+		for (int j = 0; j < 10; j++) {
+			tmp2 = nums[j];
+			if (j == 0) nums[j] = nums[j + 1];
+			else if (j == 9) nums[j] = tmp1;
+			else {
+				tmp2 = nums[j];
+				nums[j] = tmp1 + nums[j + 1];
+				tmp1= tmp2;
+			}
+			tmp1 = tmp2;
+		}
+	}
+	int		ans = 0;
+	for (int i = 0; i < 10; i++) ans += nums[i];
 	cout << ans;
 	return (0);
 }
