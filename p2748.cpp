29fe7fd44a3d5dc619ba7c0efb74be4bd0e06f69
강준현 <kangjunhyeon@gangjunhyeon-ui-MacBookPro.local>
2020-09-05 16:14:09 +0900

    Recreate p2748.cpp
    before code use recursive, so exceed time limit
    this code use queue

--- p2748.cpp
+++ p2748.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
-
-int		ft_fibo(int n)
-{
-	if (n == 0) return 0;
-	else if (n == 1) return 1;
-	return (ft_fibo(n - 1) + ft_fibo(n - 2));
-}
-	
 
 int		main(void)
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	int	n;
+	unsigned long long 		n;
+	queue<unsigned long long>	q;
 
 	cin >> n;
-	cout << ft_fibo(n);
+	if (n == 0) {
+		cout << 0;
+		return (0);
+	}
+	else if (n == 1) {
+		cout << 1;
+		return (0);
+	}
+	q.push(0);
+	q.push(1);
+	while (n-- - 1) {
+		q.push(q.front() + q.back());
+		q.pop();
+	}
+	cout << q.back();
 	return (0);
 }
