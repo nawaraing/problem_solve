03175acb9c3a3d33c3871e83a142f78abdd6fed7
강준현 <kangjunhyeon@gangjunhyeon-ui-MacBookPro.local>
2020-09-09 16:23:43 +0900

    Modify p1152.cpp
    input len is 1

--- p1152.cpp
+++ p1152.cpp
@@ -9,6 +9,10 @@ int		main(void)
 	int		ans = 0;
 
 	getline(cin, s);
+	if ((int)(s.length()) == 1 && s[0] != ' ') {
+		cout << 1;
+		return (0);
+	}
 	for (int i = 0; i < (int)s.length() - 1; i++) {
 		if (s[i] != ' ' && s[i + 1] == ' ') ans++;
 		else if (i + 1 == (int)s.length() - 1 && s[i + 1] != ' ') ans++;
