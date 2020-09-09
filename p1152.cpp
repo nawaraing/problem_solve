468dde49935c37387bb5b018f0f5603014d7dbed
강준현 <kangjunhyeon@gangjunhyeon-ui-MacBookPro.local>
2020-09-09 14:49:10 +0900

    Create p1152.cpp for ctp

---
+++ p1152.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int		main(void)
+{
+	string		s;
+	int		ans = 0;
+
+	getline(cin, s);
+	for (int i = 0; i < s.length() - 1; i++) {
+		if (s[i] != ' ' && s[i + 1] == ' ') ans++;
+		else if (i + 1 == s.length() - 1 && s[i] != ' ') ans++;
+	}
+	cout << ans;
+	return (0);
+}
