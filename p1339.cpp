91e6abc45b6b9942e8af8498738a60b5302eb4b8
junkang <nawaddaing@gmail.com>
2020-11-12 06:50:24 +0900

    Solve greedy, brute force, backtracking

--- p1339.cpp
+++ p1339.cpp
@@ -2,51 +2,49 @@
 #include <cstring>
 #include <vector>
 #include <algorithm>
-#include <list>
 
-using namespace std;				//		반례 : AAA BAA
+using namespace std;
 
-int								alpha[26], cnt, sum, num, n;
-string							str[10];
-vector<pair<int, list<char> > >	v;									//	<digit, lst>
-pair<int, list<char> >			tmp;
-list<char>						lst;
+vector<int>			v[26];
+string				str;
+int					n;
 
-bool	ft_sort(pair<int, list<char> > a, pair<int, list<char> > b) {return a.first < b.first;}
+bool	ft_sort(const vector<int> a, const vector<int> b) {
+	int			sum1 = 0, sum2 = 0;
+
+	for (int i = 0; i < 8; i++) {
+		sum1 += sum1 * 10 + a[i];
+		sum2 += sum2 * 10 + b[i];
+	}
+	return (sum1 > sum2);
+}
+
+bool	ft_int_sort(int a, int b) {return (a > b);}
 
 int		main(void)
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
+	int		sum = 0, num[10];
+
 	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cin >> str[i];
-		lst.clear();
-		for (int j = 0; j < str[i].size(); j++)
-			lst.push_back(str[i][j]);
-		v.push_back(make_pair(str[i].size(), lst));
+	for (int i = 0; i < 26; i++)
+		for (int j = 0; j < 8; j++)
+			v[i].push_back(0);
+	while (n--) {
+		cin >> str;
+		for (int i = 0; i < (int)str.length(); i++)
+			v[str[i] - 'A'][8 - (int)str.length() + i]++;
 	}
-	num = 9;
-	while (v.size()) {
-		sort(v.begin(), v.end(), ft_sort);
-		tmp = v.back();
-		v.pop_back();
-		if (!alpha[tmp.second.front() - 'A']) alpha[tmp.second.front() - 'A'] = num--;
-		if (!num) break ;
-		if (tmp.second.size()){
-			tmp.second.pop_front();
-			v.push_back(make_pair(tmp.first - 1, tmp.second));
-		}
-	}
-//	for (int i = 0; i < 26; i++) cout << alpha[i] << " ";
-//	cout << "\n";
-	for (int i = 0; i < n; i++) {
-		num = 0;
-		for (int j = 0; j < str[i].size(); j++)
-			num = 10 * num + alpha[str[i][j] - 'A'];
-		sum += num;
-	}
+	for (int i = 0; i < 10; i++) num[i] = 0;
+	sort(v, v + 26, ft_sort);
+	for (int j = 0; j < 8; j++)
+		for (int i = 0; i < 10; i++)
+			num[i] = num[i] * 10 + v[i][j];
+	sort(num, num + 10);
+	for (int i = 0; i < 10; i++)
+		sum += num[i] * i;
 	cout << sum;
 	return (0);
 }
