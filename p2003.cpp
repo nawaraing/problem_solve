e62021323d6c2dcf112524b3c5ea5e36e3261766
junkang <nawaddaing@gmail.com>
2020-11-11 22:51:24 +0900

    Solve two pointer

--- p2003.cpp
+++ p2003.cpp
@@ -3,10 +3,30 @@
 
 using namespace std;
 
+int			sum[10001];
+
 int		main(void)
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
+	int			a, b, n, m, cnt = 0;
+
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		cin >> a;
+		sum[i] = sum[i - 1] + a;
+	}
+	a = 0; b = 1;
+	while (1) {
+		if (sum[b] - sum[a] > m) a++;
+		else if (sum[b] - sum[a] < m) b++;
+		else {
+			cnt++;
+			a++;
+		}
+		if (a > 10000 || b > 10001) break ;
+	}
+	cout << cnt;
 	return (0);
 }
