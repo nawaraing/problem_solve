ba9a838390e53c152293002e9009149122cc6c1e
junkang <nawaddaing@gmail.com>
2020-11-21 00:21:51 +0900

    Unsolve p13701

--- p13701.cpp
+++ p13701.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
+
+bool		a[33554433];
 
 int		main(void)
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	vector<int>			v;
-	int					num;
-	bool				flag;
-	
-	while (scanf("%d", &num) == 1) {
-		flag = 0;
-		for (int i = 0; i < v.size(); i++) {
-			if (v[i] == num) {
-				flag = 1;
-				break ;
-			}
+	int		b;
+	bool	flag = 0;
+
+	while (!cin.eof()) {
+		cin >> b;
+		if (!a[b]) {
+			a[b] |= 1;
+			if (flag) cout << " ";
+			cout << b;
 		}
-		if (!flag) {
-			if (!v.empty()) cout << " ";
-			cout << num;
-			v.push_back(num);
-		}
+		flag = 1;
 	}
 	return (0);
 }
