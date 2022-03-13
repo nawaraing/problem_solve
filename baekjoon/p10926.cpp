a41ca1f0410b0d5e6612f5c26ae934308992b23f
nawaraing <59286081+nawaraing@users.noreply.github.com>
2022-03-13 18:29:14 +0900

    Solve implementation 10926.cpp

---
+++ baekjoon/p10926.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/*
+* Problem
+*     입력으로 주어지는 문자열 뒤에 ??!를 붙여서 출력하라
+*
+* Ideas
+*     iostream의 cin, cout을 사용하여 문제를 해결하겠다
+*     
+* Comment
+*     'warning: trigraph ignored [-Wtrigraphs]'
+*     c++에서는 '??'을 연달아서 쓸 수 없다고 한다
+*     warning을 해결하기 위해 '\?\?'으로 수정해주었다
+*/
+
+using namespace std;
+
+int main() {
+	string id;
+
+	cin >> id;
+	cout << id << "\?\?!";
+}
